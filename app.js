e9b6c66d581d955a414e0f6bdd35dca0920d938e
syafiqsaini <syafiqhusainisaharudin@gmail.com>
2020-05-09 21:18:00 +0800

    Modified listen port

--- app.js
+++ app.js
@@ -204,6 +204,6 @@ app.get('/filter', (req, res)=>{
 });
 
 // START SERVER
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server started on port 3000');
 });
